bca5ac51650450689a3370b72db3a3e418d9e8d4
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-23 12:59:14 +0300

    chore: add a route for reports

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/reports", reportRoutes);
 
 
 // Routes
